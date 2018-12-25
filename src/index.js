4e18eceb46e54b83ef3b0d5026f99d201e2bb9d5
mythLabs <amitbisht744@gmail.com>
2018-12-25 15:25:42 +0530

    Refactored schema and added update mutation

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
 const  { GraphQLServer } = require('graphql-yoga');
 
-/* The typeDefs constant defines your GraphQL schema (more about this in a bit). 
-Here, it defines a simple Query type with one field called info. This field has the type String!. 
-The exclamation mark in the type definition means that this field can never be null*/
-const typeDefs = `
-    type Query {
-        info: String!
-        feed: [Link!]!
-    }
-
-    type Link {
-        id: ID!
-        description: String!
-        url: String!
-      }
-    `
 
 /* The resolvers object is the actual implementation of the GraphQL schema. 
 Notice how its structure is identical to the structure of the type definition inside typeDefs: 
@@ -24,21 +9,28 @@ let links = [{
     url: 'www.howtographql.com',
     description: 'Fullstack tutorial for GraphQL'
   }]
+  let idCount = links.length
 
 const resolvers = {
     Query: {
        info: () => 'Very important data',
        feed: () => links
     },
-    Link: {
-        id: (parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url,
+    Mutation: {
+        post: (parent, args) => {
+           const link = {
+            id: `link-${idCount++}`,
+            description: args.description,
+            url: args.url,
+          }
+          links.push(link)
+          return link
+        }
       }
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
 })
 
